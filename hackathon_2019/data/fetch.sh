49041617fa7edb11d6e6cac5b9d9c297f60bf5f7
Theodore Kisner <tskisner.public@gmail.com>
2019-03-29 08:03:31 -0700

    Fix fetch url

--- hackathon_2019/data/fetch.sh
+++ hackathon_2019/data/fetch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ ! -f "focalplane.txt" ]; then
-    wget ...
+    curl -SL https://www.dropbox.com/s/x4j5ke5iq2ngo0w/focalplane.txt?dl=1 -o focalplane.txt
 fi
 
 if [ ! -f "boresight.txt" ]; then
-    wget ...
+    curl -SL https://www.dropbox.com/s/5pckf9qlf8h4si7/boresight.txt?dl=1 -o boresight.txt
 fi
