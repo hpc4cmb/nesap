1279f848b7bd8da9263b2fd3a11febb773c43329
Theodore Kisner <tskisner.public@gmail.com>
2019-04-30 11:43:40 -0700

    Move stream initialization to outer layer of code.

--- hackathon_2019/pointing_cuda.hpp
+++ hackathon_2019/pointing_cuda.hpp
@@ -6,6 +6,18 @@
 #include <cmath>
 #include <cstdio>
 
+#include <cuda_runtime.h>
+
+
+static void CudaError(cudaError_t err, char const * file, int line) {
+    if (err != cudaSuccess) {
+        printf("%s in %s at line %d\n", cudaGetErrorString(err),
+               file, line);
+        exit(EXIT_FAILURE);
+    }
+}
+#define CUDA_CHECK(err) (CudaError(err, __FILE__, __LINE__))
+
 namespace toast {
 
 // High-level pointing function.
@@ -18,6 +30,7 @@ void detector_pointing_healpix(
         std::map <std::string, toast::AlignedVector <double> > const & detquat,
         std::map <std::string, double> const & detcal,
         std::map <std::string, double> const & deteps,
+        int numSMs, cudaStream_t * streams,
         std::map <std::string, toast::AlignedVector <int64_t> > & detpixels,
         std::map <std::string, toast::AlignedVector <double> > & detweights);
 
